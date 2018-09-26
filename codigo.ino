8b9eb116c88196628bb6350287ef358567dcfc09
Gabrie Masson <gabrielmasson.a3912@gmail.com>
2018-09-26 20:10:00 -0300

    TOC

--- codigo.ino
+++ codigo.ino
@@ -1,38 +1,38 @@
-#include <Ultrasonic.h>   //Carrega a biblioteca Ultrasonic  
-#include <NewTone.h>    //Carrega a biblioteca Newtone  
+#include <Ultrasonic.h>   // Carrega a biblioteca Ultrasonic  
+#include <NewTone.h>    // Carrega a biblioteca Newtone  
    
-//Dados do buzzer  
+// Dados do buzzer  
 #define tempo 500   
 int frequencia = 2000;   
 int Pinofalante = 2;   
     
 int atraso = 1000;  
    
-//Define o pino do Arduino a ser utilizado com o pino Trigger do sensor  
+// Define o pino do Arduino a ser utilizado com o pino Trigger do sensor  
 #define PINO_TRIGGER 13   
-//Define o pino do Arduino a ser utilizado com o pino Echo do sensor  
+// Define o pino do Arduino a ser utilizado com o pino Echo do sensor  
 #define PINO_ECHO   10   
    
-//Inicializa o sensor ultrasonico  
+// Inicializa o sensor ultrasonico  
 Ultrasonic ultrasonic(PINO_TRIGGER, PINO_ECHO);   
    
 void setup()  
 {  
-  pinMode(Pinofalante,OUTPUT); //Pino do buzzer   
-  Serial.begin(9600); //Inicializa a serial  
+  pinMode(Pinofalante,OUTPUT); // Pino do buzzer   
+  Serial.begin(9600); // Inicializa a serial  
 }  
    
 void loop()  
 {  
   float cmMsec, inMsec;  
     
-  //Le os dados do sensor, com o tempo de retorno do sinal  
+  // Le os dados do sensor, com o tempo de retorno do sinal  
   long microsec = ultrasonic.timing();   
    
-  //Calcula a distancia em centimetros  
+  // Calcula a distancia em centimetros  
   cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);   
     
-  //Ajusta o atraso de acordo com a distancia  
+  // Ajusta o atraso de acordo com a distancia  
   if (cmMsec > 80)  
   {  
     atraso = 2000;  
@@ -54,12 +54,12 @@ void loop()
     atraso = 300;  
   }  
      
-  //Apresenta os dados, em centimetros, na Serial  
+  // Apresenta os dados, em centimetros, na Serial  
   Serial.print("Cent: ");  
   Serial.print(cmMsec);  
   Serial.print(" atraso : ");  
   Serial.println(atraso);  
-  //Emite o bip  
+  // Emite o bip  
   NewTone(Pinofalante, frequencia, tempo);   
    
   delay(atraso);  
